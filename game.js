024d2f81de817de3e62e457178d6f3602c4c083f
SaulJCer <190316056+SaulJCer@users.noreply.github.com>
2025-09-17 21:36:09 -0400

    fixed grid problem, becareful when using for loop that interact with each other, if they have the same variable it will cause problems

--- game.js
+++ game.js
@@ -7,17 +7,18 @@ const gridContainer = document.querySelector(".grid-container");
 gridContainer.style.display = "flex";
 gridContainer.style.justifyContent = 'center';
 gridContainer.style.alignItems = 'center'; //maybe not needed
+gridContainer.style.flexDirection = "column";
 
 squareGridSize = 16;
 
-function makeGridSquare() {
+function makeGridSquare(gridRow) {
     const tempGridSquare = document.createElement('div');
 
     tempGridSquare.style.width = '50px';
     tempGridSquare.style.height = "50px";
     tempGridSquare.style.border = '1.5px solid orange';
     tempGridSquare.className = "gridSqaure"
-    return tempGridSquare;
+    gridRow.appendChild(tempGridSquare);
 };
 
 function makeGridRowContainer() {
@@ -25,19 +26,21 @@ function makeGridRowContainer() {
     tempGridRow.className = 'rowContainer';
 
     for (i = 1; i <= squareGridSize; i++) {
-        tempGridRow.appendChild(makeGridSquare());
+        makeGridSquare(tempGridRow);
     }
     
     tempGridRow.style.display = "flex";
 
-    gridContainer.appendChild(tempGridRow);
+    return tempGridRow;
 };
 
 function createGrid() {
-    for (i = 1; i <= squareGridSize; i++) {
-        console.log(i);
-        makeGridRowContainer();
+    for (j = 1; j <= squareGridSize; j++) {
+        let gridRow = makeGridRowContainer();
+        gridContainer.appendChild(gridRow);
     }
 }
+
+
 
 createGrid();
